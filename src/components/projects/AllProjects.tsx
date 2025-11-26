0915e6a4959838817acdcd1a33cd493af54e24b5
priyanshsinghvi <priyanshsinghvi0308@gmail.com>
2025-11-27 02:53:11 +0530

    Updated portfolio files

--- src/components/projects/AllProjects.tsx
+++ src/components/projects/AllProjects.tsx
@@ -1,17 +1,17 @@
-"use client";
-import { Card, Carousel } from "@/components/projects/apple-cards-carousel";
-import { data } from "@/components/projects/Data";
+// File: src/components/projects/AllProjects.tsx
 
+import { Carousel, Card } from '@/components/projects/apple-cards-carousel';
+import { data } from './Data';
 
 export default function AllProjects() {
   const cards = data.map((card, index) => (
-    <Card key={card.src} card={card} index={index} layout={true} />
+    <Card key={card.title} card={card} index={index} layout={true} />
   ));
   console.log(data[0]);
 
   return (
-    <div className="w-full h-full pt-8">
-      <h2 className="max-w-7xl mx-auto text-xl md:text-3xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+    <div className="h-full w-full py-20">
+      <h2 className="mx-auto max-w-7xl pl-4 font-sans text-xl font-bold text-neutral-800 md:text-5xl dark:text-neutral-200">
         My Projects
       </h2>
       <Carousel items={cards} />
